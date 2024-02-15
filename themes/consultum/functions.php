49d0750576900eeeb4b49551e94b6f95051c2762
borodkoleg <borodkoleg@gmail.com>
2024-02-15 18:30:55 +0200

    add bootstrap

--- themes/consultum/functions.php
+++ themes/consultum/functions.php
@@ -146,6 +146,9 @@ function consultum_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+  wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/bootstrap-5.0.2-dist/js/bootstrap.min.js', array(), _S_VERSION, true );
+
 }
 add_action( 'wp_enqueue_scripts', 'consultum_scripts' );
 

6dc5dd8219f8e513dd0f291ac8d7f4e94136dbf9
borodkoleg <borodkoleg@gmail.com>
2024-03-20 15:22:37 +0200

    five-section, business-plans, rem instead px

--- themes/consultum/template-home.php
+++ themes/consultum/template-home.php
@@ -36,6 +36,71 @@ $img_src = get_template_directory_uri() . "/assets/img/";
     </div>
   </div>
 </section>
+<section class="five-section">
+  <div class="custom-containder five-block">
+    <div class="five-block-left">
+      <div class="div-2">Excellent</div>
+      <img loading="lazy" src="<?= $img_src ?>/green_starts.svg" class="img" />
+      <div class="div-3">
+        <div class="div-4">Based on</div>
+        <div class="div-5">2,156 reviews</div>
+      </div>
+      <img loading="lazy" src="<?= $img_src ?>/trust_black.svg" class="img-2" />
+    </div>
+    <?php el_box_review(
+      "Professional and Timely Service",
+      "The company impresses with its high efficiency and professionalism. They assisted me with writing a business plan and visa processing faster than I expected. I highly recommend their services",
+      "Darrell Steward",
+      "17 hour ago"
+    );
+    ?>
+    <?php el_box_review(
+      "Professional and Timely Service",
+      "The company impresses with its high efficiency and professionalism. They assisted me with writing a business plan and visa processing faster than I expected. I highly recommend their services",
+      "Darrell Steward",
+      "17 hour ago"
+    );
+    ?>
+    <?php el_box_review(
+      "Professional and Timely Service",
+      "The company impresses with its high efficiency and professionalism. They assisted me with writing a business plan and visa processing faster than I expected. I highly recommend their services",
+      "Darrell Steward",
+      "17 hour ago"
+    );
+    ?>
+    <?php el_box_review(
+      "Professional and Timely Service",
+      "The company impresses with its high efficiency and professionalism. They assisted me with writing a business plan and visa processing faster than I expected. I highly recommend their services",
+      "Darrell Steward",
+      "17 hour ago"
+    );
+    ?>
+    <?php el_box_review(
+      "Professional and Timely Service",
+      "The company impresses with its high efficiency and professionalism. They assisted me with writing a business plan and visa processing faster than I expected. I highly recommend their services",
+      "Darrell Steward",
+      "17 hour ago"
+    );
+    ?>
+  </div>
+</section>
+<section class="business-plans">
+  <div class="custom-containder business-plans-block">
+    <h2>Business plans by <span>Visa Types</span></h2>
+    <div class="box">
+      <div class="box-plan">
+        <a href="#">EB-1 Business Plan</a>
+        <a href="#">E2 Treaty Investor Visa Business Plan</a>
+        <a href="#">EB-2 NIW Business Plan</a>
+      </div>
+      <div class="box-plan">
+        <a href="#">E1 Treaty Trader Visa Business Plan</a>
+        <a href="#">L1 Visa Business Plan</a>
+        <a href="#">EB-5 Business Plan</a>
+      </div>
+    </div>
+  </div>
+</section>
 <?php
 // get_footer();
 ?>

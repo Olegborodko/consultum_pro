4ed45a5adf61b6128afa28dee369c32e6658ad5d
borodkoleg <borodkoleg@gmail.com>
2024-03-27 21:49:31 +0200

    what_is_e1 block

--- themes/consultum/template-e1.php
+++ themes/consultum/template-e1.php
@@ -26,6 +26,45 @@ $img_src = get_template_directory_uri() . "/assets/img/";
     <div class="page">E1 Visa Business Plan</div>
   </div>
 </section>
+<section class="what-is-e1">
+  <div class="custom-containder">
+    <h2>
+      What is <span>E1 Visa Business Plan?</span>
+    </h2>
+    <div class="desc">
+      The E1 treaty trader visa is an exchange Entry Permit that enables outsiders to go to the U.S. for the
+      commencement
+      of business using E1 visa. This Entry Permit grants them access to things like administration, products, and
+      anything that is a commodity.
+      <br><br>
+      This nonimmigrant Entry Permit is an ideal choice for entrepreneurs and representatives of the entrepreneurs who
+      must venture into global import or product selling.
+      <br><br>
+      The E1 visa treaty trader permits candidates from a country that has marked a deal of trade and route with the
+      U.S.,
+      and is aware of the global trade arrangement. On the other hand, the nation ought to be articulated as a trade
+      country according to the regulations in the United States.
+      <br><br>
+      Assuming you have workers, they are likewise qualified for the US E1 treaty trader visa. You should go through the
+      rundown of trade nations to see whether your nation is applicable for an E1 settlement trader visa or not. There
+      is
+      a significant exchange necessary for the Treaty dealer Entry Permit E1. This sum is set for exchange that
+      incorporates trading products, banking around the world, interchanges, and things like protection and travel
+      industry. You can likewise rely on trading the global administrations. There are no lines for any monetary worth
+      or
+      the exchanges you make.
+    </div>
+  </div>
+  <div class="pdf-block">
+    <div class="custom-containder">
+      <div class="pdf-inside">
+        <a href="" class="pdf-link"><img class="pdf-img" src="<?= $img_src ?>/PDF.webp" alt="pdf"><span>Illustrative
+            sample business plan for E1 Visa Business Plan</span><img class="upload-img"
+            src="<?= $img_src ?>/download.webp" alt="upload"></a>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php
 get_footer();
